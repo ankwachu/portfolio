52c5349f19bf6923fe91b8a8ea2ce1ccbafa7ef9
Anthony Kwan Chung <anthonykwanchung@gmail.com>
2021-09-21 10:31:03 +0200

    install bootstrap

--- assets/app.js
+++ assets/app.js
@@ -8,7 +8,13 @@
 // any CSS you import will output into a single css file (app.css in this case)
 import './styles/app.scss';
 
+const $ = require('jquery');
+
 // start the Stimulus application
 import './bootstrap';
+
+$(document).ready(function() {
+    $('[data-toggle="popover"]').popover();
+});
 
 console.log('Hello Webpack Encore !')
